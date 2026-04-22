3624726c81650496849554243f2544aefa115ff7
Sayali Sawant <sayalis2024@gmail.com>
2026-04-22 11:04:35 -0400

    Update GA tracking ID for social listening app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import ReactGA from 'react-ga4'
 
-ReactGA.initialize('G-8WKQR1P14M')
+ReactGA.initialize('G-K4KHGK52E0')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
